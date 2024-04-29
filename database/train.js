2d016c6e63d6c8f8448e1d3c5c8cfe1907acfe8d
student9876 <shouvik9876@gmail.com>
2024-04-29 19:41:14 +0530

    Adjust spaces

--- database/train.js
+++ database/train.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-
-// mongoose.connect('mongodb://localhost:27017/eticketbooking', { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => console.log('Connected to MongoDB'));
-
 const trainSchema = new mongoose.Schema({
     train_no: String,
     train_name: String,
